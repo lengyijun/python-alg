2cbd0ec37201de2dde9b6e4b6fe890912542eff5
sjtu-lyj <sjtu5140809011@gmail.com>
2015-12-04 20:49:11 +0800

    、change a lot

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 def bubble_sort(A):
     for i in range(len(A)-1,0,-1):
        for j in range(0,i):
@@ -11,17 +10,31 @@ def bubble_sort(A):
            # j=j+1
     print(A)
 
-def insert_sort(A):
-    ss=[A[0]]
-    for item in A:
-        pivot=0
-        while ss[pivot]<item:
-            pivot+=1
-        temp=ss[pivot:]#利用PYTHON的特性
-        ss[pivot]=item
-        ss.extend(temp)
-    print(A)
 
+# http://www.2liang.me/archives/257
+def insert_sort_change(A):
+    for i in range(1,len(A)):
+        j=i-1
+        key=A[i]
+        while j>=0:
+            if A[j]>key:
+                A[j+1]=A[j]
+                A[j]=key
+            j-=1
+    print A
+
+def select_sort(A):
+    ss=[]
+    i=0
+    while len(A)!=0 :
+        min_num=min(A)
+        ss.append(min_num)
+        for i in range(0,len(A)):
+            if A[i]==min_num:
+               A.pop(i)
+               break
+    print(ss)
 
 A=[49,38,65,97,76,13,27,50]
-bubble_sort(A)
+# bubble_sort(A)
+insert_sort_change(A)
