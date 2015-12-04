399372a86b7ed5374b17d34602a1740fb0366f7a
sjtu-lyj <sjtu5140809011@gmail.com>
2015-12-04 19:05:51 +0800

    modify bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,10 +4,11 @@ def bubble_sort(A):
     for i in range(len(A)-1,0,-1):
        for j in range(0,i):
            if A[j]>A[j+1]:
-               temp=A[j]
-               A[j]=A[j+1]
-               A[j+1]=temp
-           j=j+1
+               A[j],A[j+1]=A[j+1],A[j]
+           #     temp=A[j]
+           #     A[j]=A[j+1]
+           #     A[j+1]=temp
+           # j=j+1
     print(A)
 
 A=[49,38,65,97,76,13,27,50]
